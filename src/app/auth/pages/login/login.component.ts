2fbe39572e9a9b7138f3fdb628fb1e467e034cf2
DanielL <rra.daniel@gmail.com>
2022-10-31 16:46:46 -0300

    Primer Commit

---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+  ]
+})
+export class LoginComponent {
+
+  miFormulario: FormGroup = this.fb.group({
+    email:['',[]]
+  });
+
+  constructor(private fb:FormBuilder) { }
+
+
+
+}
